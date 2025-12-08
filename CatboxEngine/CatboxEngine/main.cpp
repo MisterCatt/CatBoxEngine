e71ae4ae1b0cfda1a5029d8569316130d6289ec7
Daniel Mardunovich <92152622+MisterCatt@users.noreply.github.com>
2025-12-08 17:48:04 +0100

    Making opengl display a window

---
+++ CatboxEngine/CatboxEngine/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+#include <glad/glad.h>
+#include <glfw3.h>
+
+
+int main() {
+
+	std::cout << "Welcome to Catbox Engine!" << std::endl;
+
+	if (!glfwInit()) 
+	{
+		std::cout << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window;
+	window = glfwCreateWindow(640, 480, "Catbox Engine", NULL, NULL);
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.4f, 0.3f, 0.2f, 1.0f);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+
+	glfwTerminate();
+
+		
+		return 0;
+}
